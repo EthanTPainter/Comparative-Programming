17fa8a1f2a3bef6b2cdebb59ce5f258b29893172
unknown <ethantpainter@gmail.com>
2018-02-15 01:25:27 -0500

    Added EnergyAt/Energy for Grid functionality

--- Image-Smashing/src/P1.java
+++ Image-Smashing/src/P1.java
@@ -15,20 +15,68 @@ public class P1
         Grid<Integer> g = new Grid<Integer>(new Integer[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}});
 
         System.out.println(g);
-        System.out.println(g.transpose());
-
     }
 
+    //Get Energy at Grid location
+    //energy = h_energy + v_energy
+    //h_energy = (R_left-R_right)^2 + (G_left-G_right)^2 + (B_left-B_right)^2
+    //v_energy = (R_above-R_below)^2 + (G_above-G_below)^2 + (B_above-B_below)^2
     public static int energyAt(Grid<RGB> grid, int r, int c)
     {
+        RGB left, right, above, below;
+        int energy, h_energy, v_energy;
 
-        return 0;
+        //Horizontal Energy
+        //node found on left column
+        if(c == 0) {
+            left = grid.get(r, grid.width()-1);
+            right = grid.get(r,c+1);
+        }
+        //node found on right column
+        else if(c == grid.width()-1) {
+            left = grid.get(r, c-1);
+            right = grid.get(r, 0);
+        }
+        //node not on left or right column
+        else {
+            left = grid.get(r,c-1);
+            right = grid.get(r, c+1);
+        }
+        h_energy = (int) Math.pow(left.getR() - right.getR(), 2);
+        h_energy += (int) Math.pow(left.getG() - right.getG(), 2);
+        h_energy += (int) Math.pow(left.getB() - right.getB(), 2);
+
+        //Vertical Energy
+        if(r == 0){
+            above = grid.get(grid.height()-1, c);
+            below = grid.get(r+1, c);
+        }
+        else if(r == grid.height()-1){
+            above = grid.get(r-1, c);
+            below = grid.get(0, c);
+        }
+        else {
+            above = grid.get(r-1, c);
+            below = grid.get(r+1, c);
+        }
+        v_energy = (int) Math.pow(above.getR() - below.getR(), 2);
+        v_energy += (int) Math.pow(above.getG() - below.getG(), 2);
+        v_energy += (int) Math.pow(above.getB() - below.getB(), 2);
+        //Sum all horizontal energy and vertical energy
+        energy = h_energy + v_energy;
+        return energy;
     }
 
+    //Generate a grid with
     public static Grid<Integer> energy(Grid<RGB> grid)
     {
-
-        return null;
+        Grid<Integer> newGrid = new Grid<Integer>(grid.height(), grid.width());
+        for(int a = 0; a < grid.height(); a++){
+            for(int b = 0; b < grid.width(); b++){
+                newGrid.set(a, b, energyAt(grid,a,b));
+            }
+        }
+        return newGrid;
     }
 
     public static List<Point> findVerticalPath(Grid<RGB> grid)
